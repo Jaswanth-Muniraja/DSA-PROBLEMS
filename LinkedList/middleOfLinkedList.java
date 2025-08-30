52b97219bd9ef4f7c12fc06d406c0135a67aefdf
Jaswanth Muniraja <jaswanthjashu1233@gmail.com>
2025-08-30 21:48:53 +0530

    Added middleOfLinkedList.java

---
+++ LinkedList/middleOfLinkedList.java
@@ -0,0 +1,46 @@
+class ListNode {
+    int data;
+    ListNode next;
+
+    ListNode(int data) {
+        this.data = data;
+        this.next = null;
+    }
+
+    ListNode(int data, ListNode next) {
+        this.data = data;
+        this.next = next;
+    }
+}
+
+public class middleOfLinkedList {
+
+    public static ListNode middle_of_Linked_List(ListNode head){
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast.next != null && fast.next.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        if(fast.next == null) return slow;
+        return slow.next;
+    }
+
+    public static void main(String[] args) {
+        ListNode node1 = new ListNode(10);
+        ListNode node2 = new ListNode(20);
+        ListNode node3 = new ListNode(30);
+        ListNode node4 = new ListNode(40);
+        ListNode node5 = new ListNode(50);
+
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node5;
+
+        ListNode head = node1;
+
+        ListNode middleNode = middle_of_Linked_List(head);
+        System.out.print(middleNode.data);
+    }
+}
