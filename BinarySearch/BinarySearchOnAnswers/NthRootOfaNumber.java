df35808c1b224d8b694cfd3767ccc820b09e963b
Jaswanth Muniraja <jaswanthjashu1233@gmail.com>
2025-09-15 22:07:40 +0530

    Added NthRootOfaNumber.java

--- BinarySearch/BinarySearchOnAnswers/NthRootOfaNumber.java
+++ BinarySearch/BinarySearchOnAnswers/NthRootOfaNumber.java
@@ -1,26 +1,31 @@
 package BinarySearchOnAnswers;
 
 public class NthRootOfaNumber {
-     public static int sqrt(int num){
+
+     public static int nthRoot(int num, int power){
         int low = 1;
         int high = 29;
-        int ans = -1;
         while(low <= high){
             int mid = (low + high)/2;
-            if( (mid*mid) <= num){
-                ans = mid;
+            if(pow(mid,power) == num) return mid;
+            if(pow(mid,power) <= num){
                 low = mid+1;
             }else{
                 high = mid-1;
             }
         }
-        // return high; //opposite polarity, from being a possible answer to not answer, not answer to answer
-        return ans;
+        return -1;
+    }
+    public static int pow(int num, int power){
+        return (int)Math.pow(num,power);
     }
 
     public static void main(String[] args){
         int num = 27;
-        int sqrt = sqrt(num);
-        System.out.println(sqrt);
+        int power = 3; // 3 -> 3^3 = 27
+        // int num = 64;
+        // int power = 2; // 8 -> 8^2 = 64
+        int nthRoot = nthRoot(num, power);
+        System.out.println(nthRoot);
     }
 }
