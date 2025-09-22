1d38b5047ee46c37d9542b356b2a018fed7c5d39
Jaswanth Muniraja <jaswanthjashu1233@gmail.com>
2025-09-22 19:23:40 +0530

    Modified searchIn2DArray.java

--- BinarySearch/BinarySearchOn2DArrays/searchIn2DArray.java
+++ BinarySearch/BinarySearchOn2DArrays/searchIn2DArray.java
@@ -7,8 +7,8 @@ public class searchIn2DArray {
       int high = (n*m) - 1;
       while(low <= high){
         int mid = (low + high)/2;
-        int row = mid/m;
-        int col = mid%m;
+        int row = mid/m;         // Because Every staring index of the row is multiple of 'm'. So, divide by 'm' gives correct row position. 
+        int col = mid%m;         // Similarly. % of 'm' gives the correct column position. Since every starting index is multiple of 'm'.
         if(matrix[row][col] == target) return true;
         else if(target > matrix[row][col]) low = mid+1;
         else high = mid -1;
