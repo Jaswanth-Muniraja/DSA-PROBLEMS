5911841cbca1494f8c482b204a3b916aefdc86cc
Jaswanth Muniraja <jaswanthjashu1233@gmail.com>
2025-11-07 23:14:02 +0530

    Added rightRotateByKPlaces.java

---
+++ Arrays/rightRotateByKPlaces.java
@@ -0,0 +1,43 @@
+public class rightRotateByKPlaces {
+    
+    public static void rightRotate(int[] arr, int k) {
+        int n = arr.length;
+        k = k % n; // In case k is greater than n
+        reverse(arr, 0, n - 1);
+        reverse(arr, 0, k - 1);
+        reverse(arr, k, n - 1);
+    }
+
+    private static void reverse(int[] arr, int start, int end) {
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+    // II Method
+    // public static int[] rightRotate(int[] arr, int k) {
+    //     int n = arr.length;
+    //     k = k % n; // In case k is greater than n
+    //     int[] rotatedArr = new int[n];
+    //     for (int i = 0; i < n; i++) {
+    //         rotatedArr[(i + k) % n] = arr[i]; // why (i+k) % n  because we are moving each element k places to the right
+    //     }
+    //     return rotatedArr;
+    // }
+        
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8};
+        int k = 3;
+        rightRotate(arr, k);
+        // arr = rightRotate(arr, k); // II Method
+        System.out.print("Array after right rotation by " + k + " places: ");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+
+}
